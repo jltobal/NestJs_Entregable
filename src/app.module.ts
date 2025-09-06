86b0aa3e1628389c4f5e80fb10a6955f3b883f97
jltobal <ziba.tobal@gmail.com>
2025-09-06 17:44:02 -0300

    inicio de test e2e

--- src/app.module.ts
+++ src/app.module.ts
@@ -12,7 +12,7 @@ import { AppService } from './app.service';
       host: 'localhost',
       port: 3306,
       username: 'root',
-      password: 'FullStack',
+      password: 'FullStack@2025',
       database: 'jest_entregable_db',
       entities: [__dirname + '/**/*.entity{.ts,.js}'],
       synchronize: true,
---
+++ src/notebooks/__test__/notebooks_E2E.spec.ts
@@ -0,0 +1,22 @@
+import { Test, TestingModule } from '@nestjs/testing';
+import { INestApplication } from '@nestjs/common';
+import * as request from 'supertest';
+import { AppModule } from '../../app.module';
+import { response } from 'express';
+
+describe('Controller E2E Test', () => {
+  let app: INestApplication;
+
+  beforeEach(async () => {
+    const moduleFixture: TestingModule = await Test.createTestingModule({
+      imports: [AppModule],
+    }).compile();
+
+    app = moduleFixture.createNestApplication();
+    await app.init();
+  });
+
+  it('E2E / GET', () => {
+    return request(app.getHttpServer()).get('/').expect(200);
+  });
+});
