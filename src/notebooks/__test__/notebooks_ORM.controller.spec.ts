56c53f3fc7a75bba416969712aea926a7b785c85
jltobal <ziba.tobal@gmail.com>
2025-09-05 23:44:18 -0300

    primer test de integracion

---
+++ src/notebooks/__test__/notebooks_ORM.controller.spec.ts
@@ -0,0 +1,70 @@
+import { Test, TestingModule } from '@nestjs/testing';
+import { NotebooksController } from '../notebooks.controller';
+import { NotebooksService } from '../notebooks.service';
+import { getRepositoryToken } from '@nestjs/typeorm';
+import { Notebook } from '../entities/notebook.entity';
+import { Repository } from 'typeorm';
+import { CreateNotebookDto } from '../dto/create-notebook.dto';
+
+describe('NotebooksController (Integration Test)', () => {
+  let controller: NotebooksController;
+  let service: NotebooksService;
+  let repository: Repository<Notebook>;
+
+  beforeEach(async () => {
+    const module: TestingModule = await Test.createTestingModule({
+      controllers: [NotebooksController],
+      providers: [
+        NotebooksService,
+        {
+          provide: getRepositoryToken(Notebook),
+          useValue: {
+            create: jest.fn((notebook: Partial<Notebook>) => ({
+              ...notebook,
+            })),
+            save: jest.fn(async (notebook: Partial<Notebook>) => ({
+              id: 1,
+              ...notebook,
+            })),
+            findOne: jest.fn(async ({ where }) => ({
+              id: 1,
+              ...where,
+              content:
+                where?.content ??
+                (where?.title === 'Lenovo' ? 'T-480s' : 'Test_Content'),
+            })),
+            clear: jest.fn(async () => {}),
+          },
+        },
+      ],
+    }).compile();
+
+    controller = module.get<NotebooksController>(NotebooksController);
+    service = module.get<NotebooksService>(NotebooksService);
+    repository = module.get<Repository<Notebook>>(getRepositoryToken(Notebook));
+  });
+
+  afterEach(async () => {
+    await repository.clear();
+  });
+
+  it('debería crear una notebook y guardarla en la base de datos', async () => {
+    const createDto: CreateNotebookDto = {
+      title: 'Lenovo',
+      content: 'T-480s',
+    };
+
+    const result = await controller.create(createDto);
+    expect(result).toBeDefined();
+    expect(result.title).toEqual(createDto.title);
+    expect(result.content).toEqual(createDto.content);
+
+    const savedNotebook = await repository.findOne({
+      where: { title: createDto.title },
+    });
+
+    expect(savedNotebook).toBeDefined();
+    expect(savedNotebook!.title).toEqual(createDto.title);
+    expect(savedNotebook!.content).toEqual(createDto.content);
+  });
+});
